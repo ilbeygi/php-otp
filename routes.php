48983cf618bd5ac60ca242cabe3815314ff8d011
mohammad <ilbeygi.m@gmail.com>
2022-12-30 12:47:07 +0330

    Add Routing system (laravel routing)

---
+++ routes.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Router;
+
+/** @var $router Router */
+
+$router->name('home')->get('/', function () {
+    return 'hello OTP PHP!';
+});
+
+$router->get('bye', function () {
+    return 'goodbye world!';
+});
+
+$router->group(['namespace' => 'App\Controllers', 'prefix' => 'users'], function (Router $router) {
+    $router->get('/', ['name' => 'users.index', 'uses' => 'UsersController@index']);
+    $router->post('/', ['name' => 'users.store', 'uses' => 'UsersController@store']);
+});
+
+// Redirect
+$router->get('/menu', function () use ($router) {
+    return new RedirectResponse($router->getRoutes()->getByName('home')->uri());
+});
+
+// catch-all route
+$router->any('{any}', function () {
+    return 'four oh four';
+})->where('any', '(.*)');
